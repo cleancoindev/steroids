36a1626b8e78e3cc4e2690b64846708e08049d74
allemanfredi <alle.manfredi@gmail.com>
2020-07-28 14:18:21 +0200

    ref(contracts): rm not used univ2pair interface fxs

--- contracts/IUniswapV2Pair.sol
+++ contracts/IUniswapV2Pair.sol
@@ -1,12 +1,6 @@
 pragma solidity ^0.4.24;
 
 interface IUniswapV2Pair {
-    function name() external pure returns (string memory);
-
-    function symbol() external pure returns (string memory);
-
-    function decimals() external pure returns (uint8);
-
     function totalSupply() external view returns (uint256);
 
     function balanceOf(address owner) external view returns (uint256);
@@ -16,8 +10,6 @@ interface IUniswapV2Pair {
 
     // prettier-ignore
     function transferFrom(address from, address to, uint value) external returns (bool);
-
-    function token0() external view returns (address);
 
     // prettier-ignore
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
